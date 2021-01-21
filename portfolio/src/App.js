890b1652c063be6808a29cdac8a325dc5e091044
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-01-21 18:59:09 -0500

    rough_draft

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,10 +4,52 @@ import './App.css';
 function App() {
   return (
     <div>
-      <p>
-        Matt Green,
+      <h1 id="name">
+        Matt Green
+      </h1>
+      <h2>
         Full Stack Developer
+      </h2>
+      <ul>
+        <li>
+        About
+        </li>
+        <li>
+        Projects
+        </li>
+        <li>
+        Contact
+        </li>
+      </ul>
+      
+      <p>
+         👀 <br/>
+        🤟 👍<br/>
+        👟👟
       </p>
+      
+      <h3>
+        About
+      </h3>
+      <h4>
+      Hi, I'm Matt!
+      </h4>
+      <p>
+      I'm a full stack software engineer in New York City. I draw on my love for art and design and... fun, to build interesting, user friendly projects.
+      </p>
+      <h3>
+        Projects
+      </h3>
+      <p>
+        a jaw dropping display of... my... one good project...
+      </p>
+      <h3>
+        Contact
+      </h3>
+      <p>
+        You can find me on linked in (LINK) or drop me a note. Looking forward to hearing from you!
+      </p>
+
     </div>
   );
 }
