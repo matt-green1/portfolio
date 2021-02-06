87dc0851dafa4c25d0bb938d73538a183c8b0329
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-02-05 20:02:39 -0500

    geting conditional sections to work

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -10,16 +10,39 @@ import Contact from './Components/Contact'
 //NOTE 2/3/21 -- will need to have the atate hold a "scroll" location and we move window to that location when you hgit home about etc
 //currently set up to mimic a routing but we may want to just scroll
 
+// NOTE: can't conditionally render hooks - so need ro refactor as a class based component
+// check this link out: https://linguinecode.com/post/4-techniques-conditional-render-react-props-state
+
 function App() {
   const [activeSection, setActiveSection] = useState("home")
   // active sections will be home, about, projects, contact (which is right below projects - maybe this should just be always visible - don't make people scroll...)
 
   console.log(activeSection)
+ 
+
   return (
     <div>
-      {/* currently not actually returning correct section */}
+      {console.log(`inside reutnr:" ${activeSection}`)}
       <NavBar setActiveSection={setActiveSection} />
-      { (() => {
+      {() => {if(activeSection === "home"){
+         console.log("home is working!!!")
+         return <Home/>;
+      } else if (activeSection === "about") {
+        console.log("about is working!!!") 
+        return <About/>;
+      } else if (activeSection === "projects") {
+        console.log("projects is working!!!")  
+        return <ProjectContainer/> ;
+     } else if (activeSection === "contacts") {
+      console.log("contacts is working!!!")  
+        return <Contact/>;
+     }
+      
+    
+      }}
+      
+      {/*switch statement currently not actually returning correct section */}
+      {/* { (() => {
         switch(activeSection) {
           case 'home':
             return <Home/>
@@ -37,7 +60,7 @@ function App() {
             return <p>something is not working...</p>
         }
         })
-      }
+      } */}
 
     </div>
   );
