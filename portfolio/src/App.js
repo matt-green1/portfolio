3d95b1f7d3b4730046d7b62f0b5199d1f6d866a3
matt-green1 <60438349+matt-green1@users.noreply.github.com>
2021-02-17 22:31:53 -0500

    installed semantic - working on nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,17 +4,30 @@ import NavBar from './Components/NavBar'
 import About from './Components/About'
 import ProjectContainer from './Containers/ProjectContainer'
 
-function App() {
- 
-    return (
-      <div>
-        <NavBar/>
-        <About/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <ProjectContainer/>
-      </div>
-    );
+class App extends React.Component {
+  state = {
+    currentSection: "about"
+  }  
+  
+  handleNavSection = () => {
+    if(this.state.currentSection === "about"){
+      this.setState({currentSection:"projects"})
+    } else {
+      this.setState({currentSection:"about"})
+    }
 
+  }
+
+  render() {
+      return (
+        <div>
+          <NavBar handleNavSection={this.handleNavSection}/>
+          <About/>
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          <ProjectContainer/>
+        </div>
+      );
+    }
 }
 
 export default App;
--- portfolio/src/Components/NavBar.js
+++ portfolio/src/Components/NavBar.js
@@ -1,41 +1,83 @@
 import React from 'react'
 import { Link, animateScroll } from "react-scroll"
+import { List, Button } from 'semantic-ui-react'
 
-function NavBar() {
+
+class NavBar extends React.Component {
+  
+  state = { 
+    activeNav: "about" 
+  }
  
-  return(
-     
-     <div>
-        "NavBar reporting for duty!" will eventually be aligned in right corner
-        <ul>
-          <li>
-          <Link
-              activeClass="active"
-              to="about"
-              spy={true}
-              smooth={true}
-              offset={70}
-              duration={790}
-              >
-                <button>About</button>
-            </Link>
-          </li>
-          <li>
-            <Link
-              activeClass="active"
-              to="projects"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={790}
-              // delay={100}
-              >
-                <button>Projects</button>
-            </Link>
-          </li>
-        </ul>
-    </div>
-    )
-}
+  
+  handleNav = () => {
+    
+  }
+
+  render() {   
+    let activeTest = this.handleNav()
+    console.log(`Console Logged: ${this.props.handleNavSection}`)
+    return(
+      
+      <div>
+          <List link>
+            <List.Item active={activeTest}>
+              <Link
+                activeClass="active"
+                to="about"
+                spy={true}
+                smooth={true}
+                offset={70}
+                duration={790}
+               >
+                    About
+              </Link>
+            </List.Item>
+            <List.Item as='a'>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={790}
+                // delay={100}
+               >
+                  Projects
+              </Link>
+            </List.Item>
+          </List>
+          
+          <ul>
+            <li>
+              <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={70}
+                  duration={790}
+                  >
+                    <button>About</button>
+              </Link>
+            </li>
+            <li>
+              <Link
+                activeClass="active"
+                to="projects"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={790}
+                // delay={100}
+                >
+                  <button>Projects</button>
+              </Link>
+            </li>
+          </ul>
+      </div>
+      )
+    }
+  }
 
 export default NavBar
